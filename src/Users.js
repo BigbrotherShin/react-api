64bd91547df65abb952a1c04756aa959cd977d56
BigbrotherShin <shinjhhp5@gmail.com>
2019-12-18 17:46:30 +0900

    useReducer with comment

--- src/Users.js
+++ src/Users.js
@@ -1,30 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
 import axios from 'axios';
 
+// 상태를 관리하게 될 때 useState 를 사용하는것 말고도 다른 방법이 있습니다.
+// 바로, useReducer 를 사용하는건데요, 이 Hook 함수를 사용하면 컴포넌트의 상태 업데이트 로직을 컴포넌트에서 분리시킬 수 있습니다.
+// 상태 업데이트 로직을 컴포넌트 바깥에 작성 할 수도 있고, 심지어 다른 파일에 작성 후 불러와서 사용 할 수도 있지요.
+
+// LOADING, SUCCESS, ERROR
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return {
+        loading: true,
+        data: null,
+        error: null,
+      }
+    case 'SUCCESS':
+      return {
+        loading: false,
+        data: action.data,
+        error: null,
+      }
+    case 'ERROR':
+      return {
+        loading: false,
+        data: null,
+        error: action.error,
+      }
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`)
+  }
+}
+
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
 
   const fetchUsers = async () => {
+    dispatch({ type: 'LOADING' })
     try {
-      setUsers(null);
-      setError(null);
-      setLoading(true);
       const response = await axios.get(
         'https://jsonplaceholder.typicode.com/users',
       );
-      setUsers(response.data); // response.data로 결과 조회
+      dispatch({ type: 'SUCCESS', data: response.data });
     } catch (e) {
-      console.error(e.response.status);
-      setError(e);
+      dispatch({ type: 'ERROR', error: e });
     }
-    setLoading(false);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
+
+  const { loading, data: users, error } = state;
 
   if (loading) return <div>LOADING..</div>;
   if (error) return <div>ERROR!!!</div>;
