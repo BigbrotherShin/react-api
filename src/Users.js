845b161a4db99b4ad74b10b7aff11cc59d01678f
BigbrotherShin <shinjhhp5@gmail.com>
2019-12-18 20:46:26 +0900

    useAsync with little styling

--- src/Users.js
+++ src/Users.js
@@ -2,6 +2,16 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import useAsync from './useAsync';
 import User from './User';
+import styled, { css } from 'styled-components';
+
+const UserList = styled.li`
+  &:hover {
+    cursor: pointer;
+  }
+  &:active {
+    font-weight: bold;
+  }
+`;
 
 // useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
 // 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
@@ -24,9 +34,9 @@ function Users() {
     <>
       <ul>
         {users.map(user => (
-          <li key={user.id} onClick={() => setUserId(user.id)}>
+          <UserList key={user.id} onClick={() => setUserId(user.id)}>
             {user.username} ({user.name})
-          </li>
+          </UserList>
         ))}
       </ul>
       <button onClick={refetch}>Reloading</button>
